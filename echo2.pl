dd40ec396c20a673a823a532feb419ac97c52baa
Phillip Rhodes <motley.crue.fan@gmail.com>
2018-07-12 23:23:50 -0400

    add file to be compiled with swipl -g GOAL -o OUT -c PROLOGFILE

---
+++ echo2.pl
@@ -0,0 +1,15 @@
+/* use_module(library(main)). */
+
+
+main(Argv) :-
+        echo(Argv).
+
+echo([]) :- nl.
+
+echo([Last]) :- !,
+            write(Last), nl.
+echo([H|T]) :-
+            write(H), write(' '),echo(T).
+
+/* initialization(main,now). */
+
